940aba411231b82169b79a5e1c3c4eaf4c680db2
hwarsi <yourGitHub@email.com>
2020-01-28 14:04:08 -0500

    new

---
+++ route_test.py
@@ -0,0 +1,21 @@
+import unittest
+from app import portfolio_app
+
+class TestValidator(unittest.TestCase):
+
+	# executed prior to each test
+	def setUp(self):
+		portfolio_app.testing = True
+		self.portfolio_app = portfolio_app.test_client()
+
+    # executed after each test
+def tearDown(self):
+    pass
+
+    def test_home_status_code(self):
+        result = self.portfolio_app.get('/')
+        self.assertEqual(result.status_code, 200)
+
+if __name__ == '__main__': 
+    unittest.main() 
+    
---
+++ static/js/app.js
@@ -0,0 +1,10 @@
+
+ $("#button").click(function(){
+    $("#contact_form").hide(1000);
+       });
+
+$("#sponge").click(function(){
+$("#contact_form").show(1000);
+    });
+
+
