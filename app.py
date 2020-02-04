c75f981f9f9c0d8f5522326ab246f3c9197eecd9
hwarsi <yourGitHub@email.com>
2020-02-04 18:36:06 -0500

    new

--- app.py
+++ app.py
@@ -1,10 +1,57 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
+import json
 
 portfolio_app = Flask(__name__)
 
-@portfolio_app.route("/home")
+@portfolio_app.route("/home", methods=['GET'])
 def homePage():
-    return render_template("portfolio.html")
+    myname = "Haris Warsi"
+
+    return render_template("portfolio.html", name=myname)
+
+@portfolio_app.route("/contact", methods=['GET'])
+def ContactPage():
+    myname = "Haris Warsi"
+
+    return render_template("contact.html", name=myname)
+
+@portfolio_app.route("/about", methods=['GET'])
+def AboutPage():
+    myname = "Haris Warsi"
+
+    return render_template("about.html", name=myname)
+
+@portfolio_app.route('/contactform', methods=['GET', 'POST'])
+def contactform():
+    if request.method == 'GET':
+        print('You have reached the backend!!!')
+    
+        ContactData = [
+            {   'Name': 'John',
+                'Email': 'billybob@gmail.com',
+            },
+
+            {   'Name': 'Thomas',
+                'Email': 'thomasbaslik@gmail.com',
+            },
+        ]
+
+        print(ContactData)
+        print('SENDING DATA TO FRONTEND...')
+
+        return jsonify(ContactData)
+    
+    if request.method == 'POST':
+        return jsonify('I didnt build post!')
+
+@portfolio_app.route('/postcontactform', methods=['POST'])
+def postcontactform():
+    print ('DATA IS HERE')
+    print (request.data)
+    frontend_data = request.data
+    print (frontend_data)
+
+    return jsonify('Success! Data has been processed!')
 
 if __name__ == "___main___":
     portfolio_app.run()
