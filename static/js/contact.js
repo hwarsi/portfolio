7a2cb2c76dc49c15c59033ff99556fb2a930f1fa
hwarsi <yourGitHub@email.com>
2020-02-07 18:13:32 -0500

    new

---
+++ static/js/contact.js
@@ -0,0 +1,74 @@
+
+    $(document).ready(function() {
+
+        $("#submit").click(function(){
+            var name = $("#name").val();
+            var email = $("#em").val();
+            var details = $("#details").val();
+            var numbers = [0,1,2,3,4,5,6,7,8,9]
+            var letters = "/^[a-zA-Z\s]+$/"; 
+            var message = $('#message').val();
+            var details = $("#subject").val();
+            var where = $("#where").val();
+            var reason = $("#reason").val();
+
+            // Validations
+            if (name === '' || email === '') {
+                alert("Please Fill Required Fields");
+                return false;
+            } else {
+                console.log('Your information has been received, we will contact you soon');
+            } 
+            if (!email.includes("@")) {
+                alert("Please enter your a valid email");
+                return false;
+            } 
+    
+            
+
+
+            // Ajax goes here
+            var baseURL = window.location.origin;
+            /*
+            var mainURL = baseURL + '/contactform';
+
+            $.ajax({
+                method: 'GET',
+                url: mainURL,
+                success: function(serverData) {
+                    console.log(serverData);
+                },
+                error: function(error) {
+                    console.log(error);
+                }
+            });
+        });
+    });
+
+
+    */
+        var form_data = {'name':name, 'email':email, 'where':where, 'details':details, 'reason':reason};
+        var Post_URL = baseURL + '/postcontactform';
+
+        $.ajax({
+            method: 'POST',
+            url: Post_URL,
+            data: JSON.stringify(form_data),
+            contentType: "application/json",
+            dataType: 'json',
+            success: function(msg) {
+                console.log(msg)
+                alert('SUCCESS REACHED API!');
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+   
+
+    var maxlength = 500;
+    $('textarea').keyup(function() {
+        var length = $(this).val().length;
+        var length = maxlength-length;
+        $('#characters').text(length);
+        });});});
