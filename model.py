f9255bfdfdbbc313d4043a4759ca555c95d8757d
hwarsi <yourGitHub@email.com>
2020-02-22 14:21:12 -0500

    new

--- model.py
+++ model.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 
 def connectToDB():
-    client = MongoClient('localhost', 27017) #Connects to the Mongo App
+    client = MongoClient('mongodb+srv://haris:9626602@appcluster-kmcfd.mongodb.net/portfolio_database', username='haris', password='9626602', authSource='portfolio_database',  authMechanism='SCRAM-SHA-1') #Connects to the Mongo App
     print(client)
     
     db = client.portfolio_database #database name
