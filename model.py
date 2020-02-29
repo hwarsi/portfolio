b223651e9d579c428581319508fd2c544cfef414
hwarsi <yourGitHub@email.com>
2020-02-29 13:25:06 -0500

    new

--- model.py
+++ model.py
@@ -8,5 +8,19 @@ def connectToDB():
     #Database name
     db = client.portfolio_database
     print(db)
+    
+
+    return db
+
+
+def connectToDB_2():
+    #Connects to the Mongo App
+    client = MongoClient('mongodb://hwarsi:9626602hw@cluster0-shard-00-00-0fkzd.mongodb.net:27017,cluster0-shard-00-01-0fkzd.mongodb.net:27017,cluster0-shard-00-02-0fkzd.mongodb.net:27017/remote_jobs?ssl=true&ssl_cert_reqs=CERT_NONE&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority')
+    print(client)
+    
+    #Database name
+    db = client.remote_jobs
+    print(db)
+    
 
     return db
