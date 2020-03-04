1e01a3dcfd3a8836dd950386d5679a91d4737c71
hwarsi <hwarsi@g.clemson.edu>
2020-03-04 18:28:06 -0500

    new

--- jobz.py
+++ jobz.py
@@ -1,72 +1,76 @@
-from model import connectToDB_2
+from model import connectToDB
 import pymongo
 
+job = [{ 'position': "Full Stack Engineer",
+                "company": "HRG Search",
+                "description": "The Full Stack Engineer will work with a small, fast-paced, agile software development team. This team is focused on developing and improving an enterprise cloud based suite of business critical applications. In this role, you will be working to improve existing (as well as develop new) architecture, product feature enhancements and general bug fixes. Individuals in this role will have the unique opportunity to drive the direction of the product for the organization."
+                },
 
-def insertStartData():
-      db = connectToDB_2()
-      remote_jobs = db.remote_jobs
+                {
+                    "position":'Full-Stack Engineer (JavaScript, React, Node, AWS)',
+                    "company":'Motion Recruitment',
+                    "description":'The AI and Data Software Development team of a globally recognized content and media company with a heavy software development arm is building the next-generation digital ad platform that provides a great reader experience while maximizing publisher revenue. As a member of this team, you will be building systems capable of powering the digital ad experience for some of the highest scale websites in the world. Demonstrated experience writing high quality, performant code is essential for this position.',
+                },
 
-      job = [{ 'position': "Full Stack Engineer",
-            "company": "HRG Search",
-            "description": "The Full Stack Engineer will work with a small, fast-paced, agile software development team. This team is focused on developing and improving an enterprise cloud based suite of business critical applications. In this role, you will be working to improve existing (as well as develop new) architecture, product feature enhancements and general bug fixes. Individuals in this role will have the unique opportunity to drive the direction of the product for the organization."
-            },
+                {
+                    "position":'Full Stack Developer',
+                    "company":'prestige staffing', 
+                    "description":'The Full-Stack Lead is a hands-on role with exclusive responsibility for owning module/service level design to delivery to key products. The role also involves in working with the product owner and architect to ensure compliance and overall security and quality standards of the company. Mentoring is also a key part of this role. Working on this high-volume, security intensive application, the Full-Stack Developer uses automation frameworks for testing and deployment. This individual will use their strong communication skills to work cross-functionally with teams in Atlanta and other locations, and has the ability to actively and effectively listen to hiring managers and candidates.',
+                },
 
-            {
-                "position":'Full-Stack Engineer (JavaScript, React, Node, AWS)',
-                "company":'Motion Recruitment',
-                "description":'The AI and Data Software Development team of a globally recognized content and media company with a heavy software development arm is building the next-generation digital ad platform that provides a great reader experience while maximizing publisher revenue. As a member of this team, you will be building systems capable of powering the digital ad experience for some of the highest scale websites in the world. Demonstrated experience writing high quality, performant code is essential for this position.',
-            },
+                {
+                    "position":'Lead Full-Stack Developer (JavaScript / AWS / Docker)',
+                    "company":'Flexion INC',
+                    "description":"Flexion is seeking a full-stack engineer to join an agile application development team tasked with modernizing a large-scale transactional system. The project work is primarily remote, but will require some client on-site work estimated at. We are looking for a full-stack engineer with strong application design and development skills. In this role, you must possess excellent problem-solving skills and deep technical knowledge with a strong aptitude for appreciating the focus, scope, and impact of your work in a much wider and forward-looking software landscape. You should also be able to demonstrate a history of technical leadership, excellent communication, and a positive attitude. As a remote employee in an organization adept at managing a remote workforce, individuals in these roles will have full responsibility for managing their time and work with a high degree of flexibility to enhance and improve the product",
+                },
 
-            {
-                "position":'Full Stack Developer',
-                "company":'prestige staffing', 
-                "description":'The Full-Stack Lead is a hands-on role with exclusive responsibility for owning module/service level design to delivery to key products. The role also involves in working with the product owner and architect to ensure compliance and overall security and quality standards of the company. Mentoring is also a key part of this role. Working on this high-volume, security intensive application, the Full-Stack Developer uses automation frameworks for testing and deployment. This individual will use their strong communication skills to work cross-functionally with teams in Atlanta and other locations, and has the ability to actively and effectively listen to hiring managers and candidates.',
-            },
+                {
+                    "position":"C# Software Engineer",
+                    "company":"Hazelcast",
+                    "description":"We’re far from the typical Silicon Valley story. Even though we’re headquartered in San Mateo, CA, our origins trace back to Turkey where we still have a large team presence. Nowadays, we’ve become a highly distributed family of 130+ employees across the globe. In a way, you can say we are proponents of Conway’s Law since our culture mirrors that of the distributed products we develop",
+                },
 
-            {
-                "position":'Lead Full-Stack Developer (JavaScript / AWS / Docker)',
-                "company":'Flexion INC',
-                "description":"Flexion is seeking a full-stack engineer to join an agile application development team tasked with modernizing a large-scale transactional system. The project work is primarily remote, but will require some client on-site work estimated at. We are looking for a full-stack engineer with strong application design and development skills. In this role, you must possess excellent problem-solving skills and deep technical knowledge with a strong aptitude for appreciating the focus, scope, and impact of your work in a much wider and forward-looking software landscape. You should also be able to demonstrate a history of technical leadership, excellent communication, and a positive attitude. As a remote employee in an organization adept at managing a remote workforce, individuals in these roles will have full responsibility for managing their time and work with a high degree of flexibility to enhance and improve the product",
-            },
+                {
+                    "position":'Senior Software Engineer',
+                    "company":'Twitter',
+                    "description":'As a web engineer on the team, you’ll be working to ensure Twitter for Websites embeds are the best way for publishers to tell amazing stories using Twitter content on their websites, and to develop engaging user experiences for the millions of logged-out visitors who visit our progressive web app every day. Through your work, Twitter grows beyond a great consumer app to a company whose content powers experiences across the web.',
+                },
 
-            {
-                "position":"C# Software Engineer",
-                "company":"Hazelcast",
-                "description":"We’re far from the typical Silicon Valley story. Even though we’re headquartered in San Mateo, CA, our origins trace back to Turkey where we still have a large team presence. Nowadays, we’ve become a highly distributed family of 130+ employees across the globe. In a way, you can say we are proponents of Conway’s Law since our culture mirrors that of the distributed products we develop",
-            },
+                {
+                    "position":'Lead Software Engineer',
+                    "company":'School of Motion',
+                    "description":'As Lead Engineer at School of Motion you’ll be responsible for continuing to grow and scale our learning platform as we introduce more courses and content to more students, all over the world.',
 
-            {
-                "position":'Senior Software Engineer',
-                "company":'Twitter',
-                "description":'As a web engineer on the team, you’ll be working to ensure Twitter for Websites embeds are the best way for publishers to tell amazing stories using Twitter content on their websites, and to develop engaging user experiences for the millions of logged-out visitors who visit our progressive web app every day. Through your work, Twitter grows beyond a great consumer app to a company whose content powers experiences across the web.',
-            },
+                },
 
-            {
-                "position":'Lead Software Engineer',
-                "company":'School of Motion',
-                "description":'As Lead Engineer at School of Motion you’ll be responsible for continuing to grow and scale our learning platform as we introduce more courses and content to more students, all over the world.',
+                {
+                    "position": 'Web Developer',
+                    "company": 'Ballotpedia',
+                    "description": 'Ballotpedia is seeking a full-time Web Developer to join our organization. Ballotpedia’s IT team supports the rest of the staff in making high-quality political data and unbiased encyclopedic content available to the American public by improving many aspects of Ballotpedia’s web presence and the behind-the-scenes tools used by staff. This is a full-stack role that may include various aspects of engineering, development, design, programming, architecture, testing, and tooling..',
 
-            },
+                },
 
-            {
-                "position": 'Web Developer',
-                "company": 'Ballotpedia',
-                "description": 'Ballotpedia is seeking a full-time Web Developer to join our organization. Ballotpedia’s IT team supports the rest of the staff in making high-quality political data and unbiased encyclopedic content available to the American public by improving many aspects of Ballotpedia’s web presence and the behind-the-scenes tools used by staff. This is a full-stack role that may include various aspects of engineering, development, design, programming, architecture, testing, and tooling..',
+                {
+                    "position":'iOS Developer',
+                    "company": 'Flexjobs',
+                    "description": 'We are looking for an experienced iOS developer to join our team. This person will work with our product and development teams to build quality applications, fix bugs, maintain the code, and implement updates as needed.',
 
-            },
+                },
 
-            {
-                "position":'iOS Developer',
-                "company": 'Flexjobs',
-                "description": 'We are looking for an experienced iOS developer to join our team. This person will work with our product and development teams to build quality applications, fix bugs, maintain the code, and implement updates as needed.',
+                {
+                    "position": 'Senior Data Engineer',
+                    "company": 'Thorn',
+                    "description":'You have a commitment to putting the children we serve at the center of everything you do. You have proficient software development knowledge, with experience building, growing, maintaining a variety of products, and a love for creating elegant applications using modern technologies.'}
+                    ]
 
-            },
+def insertStartData(job):
+    try:
+        db = connectToDB()
+        remote_jobs = db.remote_jobs
+        print(db)
+        print(remote_jobs)
+        x = remote_jobs.insert_many(job, ordered=False)
+    except Exception as e:
+        print(str(e))
 
-            {
-                "position": 'Senior Data Engineer',
-                "company": 'Thorn',
-                "description":'You have a commitment to putting the children we serve at the center of everything you do. You have proficient software development knowledge, with experience building, growing, maintaining a variety of products, and a love for creating elegant applications using modern technologies.'}
-                ]
-x = remote_jobs.insert_many(job, ordered=False)
-insertStartData()
-print(x)
+insertStartData(job)
--- model.py
+++ model.py
@@ -13,12 +13,3 @@ def connectToDB():
     return db
 
 
-def connectToDB_2():
-    #Connects to the Mongo App
-    client = MongoClient('mongodb://hwarsi:9626602hw@cluster0-shard-00-00-0fkzd.mongodb.net:27017,cluster0-shard-00-01-0fkzd.mongodb.net:27017,cluster0-shard-00-02-0fkzd.mongodb.net:27017/portfolio_database?ssl=true&ssl_cert_reqs=CERT_NONE&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority')
-    print(client)
-    db = client.portfolio_database
-    
-    #Database name
-    print(db)
-    return db
