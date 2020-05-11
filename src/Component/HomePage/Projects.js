4ac32dee1d9be5c7c6a12f5295a62df06f36a660
Faraz <farazfkm@gmail.com>
2020-05-11 15:12:09 -0400

    new

--- src/Component/HomePage/Projects.js
+++ src/Component/HomePage/Projects.js
@@ -11,59 +11,64 @@ class Projects extends Component {
   render() {
     return (
       <div className="backgroundProjects">
-        <h1 className="ProjectTitle">Projects</h1>
-        <div className="differentTitles">
-          <h1 className="DevSpotTitle">Social Media App</h1>
-          <h1 className="Portfolio">Portfolio</h1>
-        </div>
-        <div className="projectImages">
-          <img src={DevSpot} className="DevSpotImage"></img>
-          <img src={portfolio} className="portfolioImage"></img>
-        </div>
+        <h1 className="SectionTitle">Projects</h1>
 
-        <div className="projectImages"></div>
-        <div className="allDescriptions">
-          <div className="DevSpot">
-            <div className="DevGroupDescription">
-              <div className="howDevSpot">How was it built?</div>
-              <li className="DevSpotDescription">
-                Used Restful APIs to get users data.
-              </li>
-              <li className="DevSpotDescription">
-                Used React to create functional Components.
-              </li>
-              <li className="DevSpotDescription">
-                Use python routes and axios to utilize API's data.
-              </li>
-              <li className="DevSpotDescription">
-                Used GET and POST to update MongoDB data base on comments and
-                posts being posted or deleted.
-              </li>
-              <li className="DevSpotDescription">
-                Created a notification feature where users will be alerted if a
-                user posts on their wall or requests to add them as a friend.
-              </li>
-              <li className="DevSpotDescription">
-                Used OAuth 2.0 to authenticate users.
-              </li>
+        <div className="Projects">
+
+          <div className="ProjectBox">
+            <h1 className="ProjectTitle">Social Media App</h1>
+            <img src={DevSpot} className="ProjectImage"></img>
+            <div className="ProjectPoints">
+              <div className="ProjectTagline">How was it built?</div>
+              <ul>
+                <li className="ProjectDescription">
+                  Used Restful APIs to get users data.
+                </li>
+                <li className="ProjectDescription">
+                  Used React to create functional Components.
+                </li>
+                <li className="ProjectDescription">
+                  Use python routes and axios to utilize API's data.
+                </li>
+                <li className="ProjectDescription">
+                  Used GET and POST to update MongoDB data base on comments and
+                  posts being posted or deleted.
+                </li>
+                <li className="ProjectDescription">
+                  Created a notification feature where users will be alerted if a
+                  user posts on their wall or requests to add them as a friend.
+                </li>
+                <li className="ProjectDescription">
+                  Used OAuth 2.0 to authenticate users.
+                </li>
+              </ul>
             </div>
           </div>
-          <div className="portfolioSection">
-            <ul className="ProfileHow">How was it built?</ul>
-            <li className="PortfolioDesription">
-              Used React to create functional components.{" "}
-            </li>
-            <li className="PortfolioDesription">
-              Used Restful APIs to create weather widget.{" "}
-            </li>
-            <li className="PortfolioDesription">
-              Used Python and Axios to add contact info to MongoDB.{" "}
-            </li>
-            <li className="PortfolioDesription">
-              Deployed application to google cloud.
-            </li>
+
+          <div className="ProjectBox">
+            <h1 className="ProjectTitle">Portfolio</h1>
+            <img src={portfolio} className="ProjectImage"></img>
+            <div className="ProjectPoints">
+              <div className="ProjectTagline">How was it built?</div>
+              <ul>
+                <li className="ProjectDescription">
+                  Used React to create functional components.{" "}
+                </li>
+                <li className="ProjectDescription">
+                  Used Restful APIs to create weather widget.{" "}
+                </li>
+                <li className="ProjectDescription">
+                  Used Python and Axios to add contact info to MongoDB.{" "}
+                </li>
+                <li className="ProjectDescription">
+                  Deployed application to google cloud.
+                </li>
+              </ul>
+            </div>
           </div>
+
         </div>
+
       </div>
     );
   }
