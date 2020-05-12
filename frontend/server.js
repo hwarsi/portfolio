538dfacc3e51494f45be7b4dd84fe36153f5f8a1
Faraz <farazfkm@gmail.com>
2020-05-12 17:31:42 -0400

    new

--- frontend/server.js
+++ frontend/server.js
@@ -7,8 +7,9 @@ const app = express();
 app.use(cors());
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  console.log(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(9000);
